517203012a8fd5303a46dbbd07a138e6afb87717
linna1998 <1500012782@pku.edu.cn>
2020-05-12 10:12:49 -0400

    Add the hints in the start page

--- app/src/main/java/com/musicintonation/StartGameActivity.java
+++ app/src/main/java/com/musicintonation/StartGameActivity.java
@@ -4,8 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.musicintonation.core.GeneralInstrumentAndroid;
+import com.musicintonation.core.Instrumental;
+import com.musicintonation.core.Note;
 
 public class StartGameActivity extends AppCompatActivity {
   @Override
@@ -22,5 +27,21 @@ public class StartGameActivity extends AppCompatActivity {
         startActivity(intent);
       }
     });
+
+    GridLayout gridLayout = findViewById(R.id.center_notes);
+    final Instrumental instrumental = new GeneralInstrumentAndroid();
+
+    // add sample buttons for all notes
+    for (final Note note : Note.values()) {
+      Button button = new Button(this);
+      button.setText(note.getName());
+      button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          instrumental.beep(note.getHertz());
+        }
+      });
+      gridLayout.addView(button);
+    }
   }
 }
